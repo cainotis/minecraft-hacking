1e60c005177bb64cd59b0f43e51ebdf638b64371
Cainotis <40773170+cainotis@users.noreply.github.com>
2022-05-16 15:19:24 -0300

    first commit

---
+++ proxy_log.py
@@ -0,0 +1,63 @@
+"""
+"Quiet mode" example proxy
+Allows a client to turn on "quiet mode" which hides chat messages
+"""
+
+from twisted.internet import reactor
+from quarry.types.uuid import UUID
+from quarry.net.proxy import DownstreamFactory, Bridge
+
+
+import logging
+import struct
+
+class QuietBridge(Bridge):
+    quiet_mode = False
+
+    def packet_unhandled(self, buff, direction, name):
+        if direction == "downstream":
+            self.downstream.send_packet(name, buff.read())
+
+        elif direction == "upstream":
+            print(f"[*][{direction}] {name}")
+            self.upstream.send_packet(name, buff.read())
+
+    def packet_upstream_player_position(self, buff):
+        buf = buff.read()
+        # print(f"position : {buf.hex()}")
+
+        x, y, z, ground = struct.unpack(">dddB", buf)
+        print(f"[*] position {x} / {y} / {z} | {ground}")
+
+        self.upstream.send_packet("player_position", buf)
+
+
+class QuietDownstreamFactory(DownstreamFactory):
+    bridge_class = QuietBridge
+    motd = "Fun Server"
+    log_level = logging.DEBUG
+
+
+def main(argv):
+    # Parse options
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-a", "--listen-host", default="", help="address to listen on")
+    parser.add_argument("-p", "--listen-port", default=25565, type=int, help="port to listen on")
+    parser.add_argument("-b", "--connect-host", default="127.0.0.1", help="address to connect to")
+    parser.add_argument("-q", "--connect-port", default=25565, type=int, help="port to connect to")
+    args = parser.parse_args(argv)
+
+    # Create factory
+    factory = QuietDownstreamFactory()
+    factory.connect_host = args.connect_host
+    factory.connect_port = args.connect_port
+
+    # Listen
+    factory.listen(args.listen_host, args.listen_port)
+    reactor.run()
+
+
+if __name__ == "__main__":
+    import sys
+    main(sys.argv[1:])
